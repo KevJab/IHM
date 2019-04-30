adfb8a26569be9f6bed5400fafd31ca98a0d03a8
Titanmito <123clement@orange.fr>
2019-04-30 18:40:50 +0200

    menu all commande et commande fait, TODO faire fonctionner les boutons

--- launchProjet.py
+++ launchProjet.py
@@ -12,11 +12,14 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         """
         COMMANDE POUR TEST
         """
-        f = Plat("Frite",1,[patate],[],"",[])
-        a = Plat("Kebab",6,[viande,salade,tomate,oignon],[],"",[f])
+        frite = Plat("Frite",1,[patate],[],"",[])
+        kebab = Plat("Kebab",6,[viande,salade,tomate,oignon],[],"",[frite])
+        poulet = Plat("Poulet",1,[],[],"",[])
+        crevette = Plat("Crevette",1,[],[],"",[])
         coca = Boisson("Coca",1,[],[],"",False,25)
-        m = Menu("Kebab_Frites",[a],coca,6.5)
-        self.commande = Commande([m], [f, a], [coca], None)  # Commande pour tester
+        pepsi = Boisson("Pepsi",1,[],[],"",False,25)
+        m = Menu("Kebab_Frites",[kebab, frite],coca,6.5)
+        self.commande = Commande([m], [poulet, crevette], [pepsi], None)  # Commande pour tester
         self.all_commandes = [self.commande, deepcopy(self.commande), deepcopy(self.commande)]
         self.vue = "Commande"
         super(MainWindow, self).__init__()
@@ -25,15 +28,53 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         if(self.vue == "Commande"):
             frame = self.findChild(QFrame,"frameVueCommandes")
             frame.setEnabled(True)
+            obj = QVBoxLayout(frame)
+            self.textEdit = QTextEdit(self)
+            self.textEdit.setContentsMargins(0,0,0,0)
+            s = ""
+            total = 0
+
+            s += "Boissons \n ----------------------------------\n"
+            if self.commande.boissons:
+                for n in self.commande.boissons:
+                    s+= n.nom + "\t\t\t" + str(n.prix) + "€\n"
+                    total += n.prix
+            s += "\n\nPlats \n ----------------------------------\n"
+            if self.commande.plats:
+                for m in self.commande.plats:
+                    s += m.nom + "\t\t\t" + str(m.prix) + "€\n"
+                    total += m.prix
+            s += "\n\nMenus \n ----------------------------------\n"
+            if self.commande.menus:
+                for o in self.commande.menus:
+                    s += o.nom + "\t\t\t" + str(o.prix) + "€\n"
+                    total += o.prix
+
+            s += "\n\n\n\nTotal \n -------------------------------------------------------------------\n\t\t\t" + str(total) + "€"
+
+            self.textEdit.setText(s)
+            self.textEdit.adjustSize()
+            btn = QPushButton('Payer', self)
+            btn.resize(50, 50)
+            btn.setStyleSheet("background-color: yellow;")
+            obj.addWidget(self.textEdit)
+            obj.addWidget(btn)
+
+        if(self.vue == "All_Commande"):
+            frame = self.findChild(QFrame,"frameVueCommandes")
+            frame.setEnabled(True)
             obj = QHBoxLayout(frame)
 
+
             for k in self.all_commandes:
+                obj2 = QVBoxLayout()
                 self.textEdit = QTextEdit(self)
                 self.textEdit.setContentsMargins(0,0,0,0)
                 s = ""
                 if k.menus:  # Je rassemble tous les plats et les boissons pour que ce soit plus lisible
                     for i in k.menus:
-                        k.plats.append(i.plats[0])
+                        for j in i.plats:
+                            k.plats.append(j)
                         k.boissons.append(i.boisson)
                 s += "Boissons \n ----------------------------------\n"
                 if k.boissons:
@@ -45,9 +86,14 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                         s += m.nom + "\n"
                 self.textEdit.setText(s)
                 self.textEdit.adjustSize()
-                obj.addWidget(self.textEdit)
+                btn = QPushButton('Terminer', self)
+                btn.resize(50, 50)
+                btn.setStyleSheet("background-color: red;")
+                obj2.addWidget(self.textEdit)
+                obj2.addWidget(btn)
+                obj.addLayout(obj2)
 
-            print("Bla")
+            #TODO enlever la commande quand le bouton "Terminer" est appuyé
 
         elif(self.vue == "Plats"):
             frame = self.findChild(QFrame,"frameVueCommandes")
